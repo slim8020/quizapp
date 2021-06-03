7c542367241ce502f3a6ddc622a085852e02aca7
slim8020 <slim8020@naver.com>
2021-06-03 13:27:10 +0900

    Replace parameters with props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <Quiz/>
+        <h1>Today's Quiz</h1>
+        <div>
+        <input id="prev" type="button" value="prev"/>
+        <input id="next" type="button" value="next"/>
+        </div>
+
+        <Quiz imageFile='../sample.png' question='알프스 소녀 하이디 에서 하이디가 앓고 있는 병은 무엇인가요?' ch1='꾀병' ch2='상사병' ch3='몽유병' answer='1'/>
+
     </div>
   );
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -16,7 +16,7 @@ class Quiz extends Component {
     }
 
     handleSubmit(event){
-        let ans = "3";
+        let ans = this.props.answer;
         if(this.state.choice === ans){
             this.setState({answer: 'Correct!'});
         }
@@ -29,28 +29,25 @@ class Quiz extends Component {
     render() {
         return (
             <div>
-                <h1>Today's Quiz</h1>
-                <img src="../sample.png"/>
-                <h2>알프스 소녀 하이디 에서 <br/>하이디가 앓고 있는 병은 무엇인가요?</h2>
+                <img src={this.props.imageFile}/>
+                <h2>{this.props.question}</h2>
 
                 <ul>
                 <li>
-                    <label><input type="radio" name="choice" value="1" onChange={this.handleChange}/>꾀병</label>
+                    <label><input type="radio" name="choice" value="1" onChange={this.handleChange}/>{this.props.ch1}</label>
                 </li>
                 <li>
-                     <label><input type="radio" name="choice" value="2" onChange={this.handleChange}/>상사병</label>
+                     <label><input type="radio" name="choice" value="2" onChange={this.handleChange}/>{this.props.ch2}</label>
                 </li>
                 <li>
-                     <label><input type="radio" name="choice" value="3" onChange={this.handleChange}/>몽유병</label>
+                     <label><input type="radio" name="choice" value="3" onChange={this.handleChange}/>{this.props.ch3}</label>
                 </li>
                 </ul>
 
                 <div>
-                <input id="prev" type="button" value="prev"/>
                 <input id="answer" type="submit" value="submit" onClick={this.handleSubmit}/>
-                <input id="next" type="button" value="next"/>
                 </div>
-                
+
                 <p>{this.state.answer}</p>
             </div>
         );
